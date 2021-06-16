e8a1a0a42f79974a1a6b46d34455b65664af0388
NikczemnyOkularnik <grzegorzbrodziakgd@gmail.com>
2021-06-16 21:16:32 +0200

    Added constructors

--- Tasks/main.cpp
+++ Tasks/main.cpp
@@ -8,4 +8,10 @@ class Ship
     float speed_;
     short int maxCrew_;
     float capacity_;
+
+    public:
+    void setName(const std::string& name) {name_ =  name;}
+    std::string getName() const {return name_;}
+    Ship(): id_(-1) {}
+    Ship(int id_, std::string name, float speed_, short int maxCrew_, float capacity_);
 };
